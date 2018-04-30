042756c0ac6b4f3d58ba90bd7c16d463f05f22fa
Ditempo <gsw2205@gmail.com>
2018-04-30 12:36:58 +0900

    db

--- Gaida_Server/app.js
+++ Gaida_Server/app.js
@@ -25,19 +25,56 @@ app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.use('/users', require('./routes/users'));
-
  
-let db = require('./models'); 
+//Connecting DB
+const models = require("./models");
+models.sequelize.authenticate().then(() => {
+    console.log('Connected to SQL database');
+})
+.catch(err => {
+    console.error('Unable to connect to SQL database:', err);
+});
+if(CONFIG.app==='development'){
+    models.sequelize.sync();//creates tables from models
+    // models.sequelize.sync({ force: true });//good for testing
+}
 
-db.sequelize
-//   .sync() // 테이블 생성
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/', function(req, res){
+    res.statusCode = 200;//send the appropriate status code
+    res.json({status:"success", message:"Parcel Pending API", data:{}})
+ });
+ 
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+ });
+ 
+ // error handler
+ app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
+ });
 
 http.createServer(app).listen(PORT1, () => {
     console.log(`Node.js http Server running on port ${PORT1}`)
--- Gaida_Server/models/index.js
+++ Gaida_Server/models/index.js
@@ -1,15 +1,14 @@
-'use strict';
 const fs = require('fs'),
   path = require('path'),
   Sequelize = require('sequelize'),
-  config = require('../config/config');
+  config = require('../config/config').DB_CONFIG;
+
 let db = {};
-let dbConfig = config[config.env];
 const sequelize = new 
-Sequelize(dbConfig.database, dbConfig.username,
-  dbConfig.password, {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
+Sequelize(config.database, config.username,
+  config.password, {
+    host: config.host,
+    dialect: config.dialect,
     operatorsAliases: false,
     pool: {
       max: 5,
@@ -17,18 +16,26 @@ Sequelize(dbConfig.database, dbConfig.username,
       acquire: 30000,
       idle: 10000
     }
-  } ); 
-fs.readdirSync(__dirname).filter(function (file) {
-  return (file.indexOf('.') !== 0) && (file !== 'index.js');
-}).forEach( (file) => {
-  const model = sequelize['import'](path.join(__dirname, file));
-  db[model.name] = model;
-});
-Object.keys(db).forEach( (modelName) => {
-  if ('associate' in db[modelName]) {
+  } 
+);
+
+fs
+  .readdirSync(__dirname)
+  .filter((file) => {
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
+  })
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
 });
+
 db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
+db.Sequelize = Sequelize;
+
 module.exports = db;
