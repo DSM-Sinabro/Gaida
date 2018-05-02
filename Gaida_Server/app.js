a575d9435f12a6fa56dc3601495a26893fa90136
Ditempo <gsw2205@gmail.com>
2018-05-02 23:32:16 +0900

    login, create

--- Gaida_Server/app.js
+++ Gaida_Server/app.js
@@ -2,8 +2,10 @@ const express = require('express'),
     http = require('http'),
     https = require('https'),
     morgan = require('morgan'),
+    path = require('path');
     bodyParser = require('body-parser'),
-    fs = require('fs');
+    fs = require('fs'),
+    mongoose = require('mongoose');
 
 // const options = {
 //     key : fs.readFileSync('./config/key.pem'),
@@ -12,32 +14,28 @@ const express = require('express'),
 
 
 const app = express();
-// app.use(express.urlencoded());
 
-const config = require('./config/config');
+const config = require('./config');
 
 // const PORT = config.PORT || 3000;
 const PORT1 = 3000;
 // const PORT2 = 3001;
 
 app.use(morgan('dev'));
-app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-// app.use('/users', require('./routes/users'));
- 
-//Connecting DB
-const models = require("./models");
-models.sequelize.authenticate().then(() => {
-    console.log('Connected to SQL database');
-})
-.catch(err => {
-    console.error('Unable to connect to SQL database:', err);
+// Connecting DB
+mongoose.connect(config.MONGO_URI)
+    .then((response)=> {
+        console.log('Successfully connected to mongodb on : '+config.MONGO_URI);
+    })
+    .catch((error)=>{
+        console.log('Error: Could not connect to MongoDB : '+error);
+    })
+mongoose.connection.on('error', function(err) {
+  console.log('Error: Could not connect to MongoDB.');
 });
-if(CONFIG.app==='development'){
-    models.sequelize.sync();//creates tables from models
-    // models.sequelize.sync({ force: true });//good for testing
-}
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
@@ -53,32 +51,42 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/', function(req, res){
-    res.statusCode = 200;//send the appropriate status code
-    res.json({status:"success", message:"Parcel Pending API", data:{}})
- });
- 
- // catch 404 and forward to error handler
- app.use(function(req, res, next) {
-   var err = new Error('Not Found');
-   err.status = 404;
-   next(err);
- });
- 
- // error handler
- app.use(function(err, req, res, next) {
-   // set locals, only providing error in development
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
-   // render the error page
-   res.status(err.status || 500);
-   res.render('error');
- });
+// // app.get('/:email', function(req, res){
+// //     const email = req.params.email;
+// //     // res.status(200).json(email);
+// //     res.send(email);
+// //  });
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
 
+// // error handler
+// app.use(function(err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
+
+const router = require('./routes');
+app.use('/',router);
+// let user = require('./controllers/auth.controller');
+// app.get('/checkEmail/:email', (req, res) => {
+//     res.status(200).json("fasd");
+// });
+
+// app.listen(PORT1,() => {
+//     console.log("express test server");
+// })
 http.createServer(app).listen(PORT1, () => {
-    console.log(`Node.js http Server running on port ${PORT1}`)
-})
+    console.log(`Node.js http Server running on port ${PORT1}`);
+});
 // https.createServer(options, app).listen(PORT2, () => {
 //     console.log(`Node.js https Server running on port ${PORT2}`);
 // });
