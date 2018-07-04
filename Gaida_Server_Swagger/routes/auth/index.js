264dacd02625f129176b53ecffe564ef36d1032e
Ditempo <gsw2205@gmail.com>
2018-07-04 10:38:59 +0900

    make it better

--- Gaida_Server_Swagger/routes/auth/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-let router = express.router;
-
-let auth = require('./auth.controller');
-
-router.route('/', )
-module.exports = router;
--- Gaida_Server_Swagger/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-let router = express.router;
-
-let auth = require('./auth');
-
-router.use('/', auth);
-
-module.exports = router;
